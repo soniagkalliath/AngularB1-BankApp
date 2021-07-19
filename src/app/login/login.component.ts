7daa387c812f0b9981c7701bb74a533780c78ae7
soniagkalliath <soniageorge111@gmail.com>
2021-07-19 11:28:56 +0530

    Bank App - login created, one way binding

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  aim="Welcome to SBL Bank"
+
+  acNumber="Account Number Please"
+
+  acno=""
+
+  pswd=""
+
+  users:any = {
+    1000: { acno: 1000, username: "Aahil", password: "userone", balance: 5000 },
+    1001: { acno: 1001, username: "Abhinav", password: "usertwo",  balance: 6000 },
+    1002: { acno: 1002, username: "Laisha", password: "userthree",  balance: 7000 },
+    1003: { acno: 1003, username: "Riddhieka", password: "userfour", balance: 8000 }
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  accNumber(event:any){
+//console.log(event.target.value)
+this.acno = event.target.value
+  }
+
+  pswdChange(event:any){
+    //console.log(event.target.value)
+    this.pswd = event.target.value
+      }
+
+login(){
+
+var acno = this.acno;
+var pswd = this.pswd;
+
+let accDetails = this.users
+
+if(acno in accDetails){
+
+  if(pswd == accDetails[acno]["password"]){
+
+    alert("Login successful")
+  }
+  else{
+    alert("Invalid password")
+  }
+}
+else{
+  alert("Invalid Account Number")
+}
+  
+}
+
+}
